7f6ee6840a3c14b93dfa7c59e23b3635d8238ea5
EC2 Default User <ec2-user@ip-172-16-138-116.ec2.internal>
2023-06-15 13:34:40 +0000

    update test file

--- test/end_to_end.py
+++ test/end_to_end.py
@@ -1,10 +1,13 @@
+import sys, os
+sys.path.insert(0, os.path.abspath('.'))
+print(os.path.abspath('.'))
 import pandas as pd
 
 # from ing.src import S3Access
-from ing.src import ing
+from src import ing
 
 S3_INPUT_BRANDWATCH_DIR = 's3://mips-main/initial_data_collection/raw_data/brandwatch'
-S3_NEWS_DOMAIN_TUFM_FILE = "s3://mips-main-2/UFTM_classification-v2/news_table-v2-UT60-FM5.csv"
+S3_NEWS_DOMAIN_TUFM_FILE = "s3://mips-main-2/UFTM_classification-v2/news_table-v3-UT60.csv"
 
 
 def read_domain_to_class_classification():
@@ -14,15 +17,21 @@ def read_domain_to_class_classification():
     news_domain_to_tufm_class = tufmdf.set_index('news_domain')['tufm_class'].to_dict()
     return news_domain_to_tufm_class
 
-
+print("1")
 news_domain_to_tufm_class = read_domain_to_class_classification()
 
+print("2")
 news_domain_identifier = ing.NewsDomainIdentifier(news_domain_to_tufm_class.keys())
 
-bd = ing.BrandwatchData(S3_INPUT_BRANDWATCH_DIR)
+print("3")
+bd = ing.BrandwatchData(S3_INPUT_BRANDWATCH_DIR, True)
+print(bd)
 
+print("4")
 bw_df = bd.get_data()
+print(bw_df)
 
+print(bw_df.info())
 
 #
 # s3a = S3Access()
