fa9a41ce7709f288e45f1462535545ad0d1642eb
deamonpog <pog666@gmail.com>
2023-07-13 14:30:28 -0400

    made remove duplicate msgs to consider platform when identifying unique msgs. files read are stricted to have *.csv* naming signature.

--- src/ing/any_data_source_reader.py
+++ src/ing/any_data_source_reader.py
@@ -33,19 +33,19 @@ class AnyDataSourceReader:
         Removes duplicates based on the source_msg_id ("URL") column.
         """
         result_df = pd.concat([self.read_data_file(data_file) for data_file in in_file_path_list])
-        result_df.drop_duplicates(subset='source_msg_id', keep="first", inplace=True)
+        result_df.drop_duplicates(subset=["source_msg_id", "platform"], keep="first", inplace=True)
         result_df.reset_index(drop=True, inplace=True)
         return result_df
 
     def get_file_paths_list(self, in_source_folder, in_is_from_s3=False, in_s3_object: s3fs.S3FileSystem = None) -> \
     List[str]:
         """
-        Get list of all "*.csv" files form the source_folder path.
+        Get list of all "*.csv*" files form the source_folder path.
         """
         data_files = []
         prefix_path = ''
         if in_is_from_s3:
-            data_files = in_s3_object.glob(os.path.join(in_source_folder, "*"))
+            data_files = in_s3_object.glob(os.path.join(in_source_folder, "*.csv*"))
             prefix_path = 's3://'
         else:
             data_files = glob.glob(os.path.join(in_source_folder, "*"))
