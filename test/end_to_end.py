2c7ecaf6e589e4590d41f18260bc409e03c263b2
EC2 Default User <ec2-user@ip-172-16-86-157.ec2.internal>
2023-07-24 16:01:03 +0000

    use directory list instead on one dir

--- test/end_to_end.py
+++ test/end_to_end.py
@@ -1,9 +1,9 @@
 
-""" Include containing DATA_DIRECTORY for testing """
+""" Include containing folder for testing """
 import sys, os
 import datetime
 
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('/home/ec2-user/SageMaker/ing/src'))
 print(os.path.abspath('.'))
 # ---------------------------------------
 
@@ -22,8 +22,13 @@ Test in aws:
     Uncomment the lines below to test on aws sagemaker. 
     Otherwise keep them commented.
 """
-from src import ing
+import ing
 # -----------------------------
+
+# s3 specific libraries
+import s3fs
+import boto3
+s3 = s3fs.S3FileSystem(anon=False)
 
 
 """
@@ -32,20 +37,26 @@ Usual module/package imports go below here
 import pandas as pd
 # -----------------------------
 
-DATA_DIRECTORY = "C:\\STUFF\\RESEARCH\\smyrna\\Smyrna\\data_collection\\test_ing"
-NEWS_DOMAIN_TO_CLASS_FILE = "C:\\STUFF\\RESEARCH\\smyrna\\Smyrna\\data_collection\\news_table-v3-UT60.csv"
+DATA_DIRECTORY_LIST = ["s3://mips-main/initial_data_collection/raw_data/brandwatch/", "s3://mips-main/initial_data_collection/TE_ready_data/V2/"]
+NEWS_DOMAIN_TO_CLASS_FILE = "s3://mips-main-2/UFTM_classification-v2/news_table-v3-UT60.csv"
 START_DATE = datetime.datetime(2018, 3, 1, tzinfo=datetime.timezone.utc)
-END_DATE = datetime.datetime(2018, 5, 1, tzinfo=datetime.timezone.utc)
+END_DATE = datetime.datetime(2018, 5, 2, tzinfo=datetime.timezone.utc)
 FREQUENCY = '6H'
 MIN_PLAT_SIZE = 30
 MIN_ACTIVITY_PER_MONTH = 15
+
 
 
 if __name__ == "__main__":
 
     # Read input files
     any_source_reader = ing.AnyDataSourceReader()
-    paths = any_source_reader.get_file_paths_list(DATA_DIRECTORY)
+    # paths = any_source_reader.get_file_paths_list(DATA_DIRECTORY)
+    
+    paths = []
+    for data_dir in DATA_DIRECTORY_LIST:
+        paths += s3.glob(os.path.join(data_dir, "*.csv*"))
+    paths = [f"s3://{p}" for p in paths]
     print(paths)
     all_osn_msgs_df = any_source_reader.read_files_list(paths)
     print(all_osn_msgs_df)
@@ -68,7 +79,12 @@ if __name__ == "__main__":
 
     print("calculating te...")
     te_calculator = ing.TransferEntropyCalculator(data_manager)
-    te_df = te_calculator.calculate_te_network(datetime.datetime(2018, 3, 5, tzinfo=datetime.timezone.utc), datetime.datetime(2018, 3, 10, tzinfo=datetime.timezone.utc), FREQUENCY)
+    current_start_date = START_DATE
+    current_end_date = END_DATE
+    te_df = te_calculator.calculate_te_network(current_start_date, current_end_date, FREQUENCY)
 
     print("all done!")
+    
+    compression_options = dict(method='zip', archive_name='actor_te_edges_df.csv')
+    te_df.to_csv("actor_te_edges_df.csv.zip", index=False, compression=compression_options)
 
