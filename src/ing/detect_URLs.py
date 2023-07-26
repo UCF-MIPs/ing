ef2fb54662b7b4b2b5728811590e72e420a473d7
mahrab <barham@knights.ucf.edu>
2023-07-26 11:32:04 -0400

    updated URL detection code to strip html tags occurring in links

--- src/ing/detect_URLs.py
+++ src/ing/detect_URLs.py
@@ -1,17 +1,15 @@
-
+import re
 
 def detect_URLs(s):
 	# input: s, a string
 	# output: a list of strings, each containing a URL
-	tokens = s.split()
-	urls = []
-	for token in tokens:
-		if token.find("://") != -1: # check for the :// that seperates the protocol from the subdomain first
-			urls.append(token)
-		else:
-			t = [x for x in token.split(".") if x != ''] # even a shortened url must contain at least one period, separating the domain name from the domain extension
-			if len(t) > 1:
-				urls.append(token)
+
+	html_pattern = r"<(?:/?&?!?\w)+>"
+	html_strip = re.sub(html_pattern, '', s)
+
+	url_pattern = r"(?:[A-z+.-]+://)?[A-z0-9]+\.[^<>\"#{}|\\^~[\]`\s]+"
+	prog = re.compile(url_pattern)
+	urls = prog.findall(html_strip)
 	return urls
 
 if __name__ == '__main__':
