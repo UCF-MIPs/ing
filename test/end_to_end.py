fc03d41723b0a45d2e65bdf4883551f879c99a1c
EC2 Default User <ec2-user@ip-172-16-103-27.ec2.internal>
2024-03-04 15:24:50 +0000

    udated end_to_end.py for running multiple datasets

--- test/end_to_end.py
+++ test/end_to_end.py
@@ -38,114 +38,124 @@ Usual module/package imports go below here
 """
 import pandas as pd
 import glob
+
+from elbow_function import get_elbow
 # -----------------------------
 
-# DATA_DIRECTORY_LIST = ["s3://mips-main/initial_data_collection/raw_data/brandwatch/", "s3://mips-main/initial_data_collection/TE_ready_data/V2/"]  # DONE
-DATA_DIRECTORY_LIST = ["s3://mips-phase-2/initial_query_data/raw_data/brandwatch/"]
-# DATA_DIRECTORY_LIST = [ "C:/STUFF/RESEARCH/Brandwatch/mariupol_hospital_clean", "C:/STUFF/RESEARCH/smyrna/Smyrna/data_collection/test_ing"]
-# DATA_DIRECTORY_LIST = ["C:/STUFF/RESEARCH/smyrna/Smyrna/data_collection/test_ing/indv"]
 
+# ------------------- COMMON -------------------
 NEWS_DOMAIN_TO_CLASS_FILE = "s3://mips-main-2/UFTM_classification-v2/news_table-v3-UT60.csv"
-# NEWS_DOMAIN_TO_CLASS_FILE = "C:/STUFF/RESEARCH/Brandwatch/news_table-v3-UT60.csv"
-
-START_DATE = datetime.datetime(2022, 1, 1, tzinfo=datetime.timezone.utc)
-END_DATE = datetime.datetime(2022, 5, 1, tzinfo=datetime.timezone.utc)
-FREQUENCY = '12H'
-MIN_PLAT_SIZE = 500
-MIN_ACTIVITY_PER_MONTH = 30
-MIN_TOTAL_RECEIVED_RETWEETS = 15
-WINDOW_SHIFT_BY_DAYS = 1
-INIT_WINDOW_SIZE_BY_DAYS = 1
-AS_GROWING = True
-
-paths = []
-for data_dir in DATA_DIRECTORY_LIST:
-    paths += s3.glob(os.path.join(data_dir, "*.csv*"))
-paths = [f"s3://{p}" for p in paths]
-# print(paths)
-
 news_domain_classes_df = pd.read_csv(NEWS_DOMAIN_TO_CLASS_FILE,
                                      usecols=['Domain', 'tufm_class', 'Language'])
 news_domain_classes_df.rename(columns={'Domain': 'news_domain', 'tufm_class': 'class', 'Language': 'lang'},
                               inplace=True)
 
-if __name__ == "__main__":
+required_dirs = ["./OUTPUTS", "./OUTPUTS/datasets"]
+for target_dir in required_dirs:
+    if not os.path.exists(target_dir):
+        os.mkdir(target_dir)
+        
+OUTPUT_DIR = "./OUTPUTS/datasets"
 
-    required_dirs = ["./OUTPUTS", "./OUTPUTS/dynamic", "./OUTPUTS/dynamic/growing", "./OUTPUTS/dynamic/moving"]
+def calculate_te_for_dataset(in_dataset_name,
+                             in_data_file_path_list,
+                             in_start_datetime,
+                             in_end_datetime,
+                             in_frequency = '12H',
+                             in_min_plat_size = 200,
+                             in_min_msg_count = None,
+                             in_window_shift_by_days = 1,
+                             in_init_window_shift_by_days = 1):
+    
+    if not in_start_datetime < in_end_datetime:
+        print(f"Error: Start date ({in_start_datetime}) is not less than End date ({in_end_datetime}) !")
+        return None
+    
+    dataset_output_dir = f"{OUTPUT_DIR}/{in_dataset_name}"
+    required_dirs = [f"{dataset_output_dir}", 
+                     f"{dataset_output_dir}/dynamic", 
+                     f"{dataset_output_dir}/dynamic/growing", 
+                     f"{dataset_output_dir}/dynamic/moving"]
     for target_dir in required_dirs:
         if not os.path.exists(target_dir):
             os.mkdir(target_dir)
-
+            
     # let data manager handle data
-    data_manager = ing.DataManager("./OUTPUTS")
-    data_manager.read_data_files(paths)
-
+    data_manager = ing.DataManager(dataset_output_dir)
+    data_manager.read_data_files(in_data_file_path_list)
+        
+    if in_end_datetime <= data_manager.all_osn_msgs_df.datetime.min() or data_manager.all_osn_msgs_df.datetime.max() <= in_start_datetime:
+        print("Error: The provided date range ({} to {}) is not within the range of the dataset ({} to {})!".format(in_start_datetime, 
+                                                                                                                     in_end_datetime, 
+                                                                                                                     data_manager.all_osn_msgs_df.datetime.min(),
+                                                                                                                     data_manager.all_osn_msgs_df.datetime.max()))
+        return None
+    
     # preprocess
-    data_manager.preprocess(news_domain_classes_df, START_DATE, END_DATE)
+    data_manager.preprocess(news_domain_classes_df, in_start_datetime, in_end_datetime)
 
     # generate tables
-    min_msg_count = MIN_ACTIVITY_PER_MONTH * (END_DATE - START_DATE).days // 30
-
-    print(f"minimum message count : {min_msg_count}")
-
-    data_manager.generate_data_tables(MIN_PLAT_SIZE, min_msg_count)
-
-    print(f"Start: {START_DATE} \nEnd: {END_DATE}")
-    print(f"Period length: {END_DATE - START_DATE}")
-    print(f"Expected actor min messages per month: {MIN_ACTIVITY_PER_MONTH}")
-    print(f"Min msg count: {min_msg_count}")
-    print(f"Users with more than {min_msg_count} messsages posted\n",
-          data_manager.all_users_df[data_manager.all_users_df["msgs_count"] > min_msg_count].reset_index())
-
-    print(f"Platform Actors with more than {MIN_PLAT_SIZE} users\n",
-          data_manager.actors_df[(data_manager.actors_df["actor_type"] == "plat") &
-                                 (data_manager.actors_df["num_users"] > MIN_PLAT_SIZE)].reset_index())
-
-    actor_id_list = data_manager.indv_actors_df[(data_manager.indv_actors_df["msgs_count"] > min_msg_count) & (data_manager.indv_actors_df["received_share_count"] > MIN_TOTAL_RECEIVED_RETWEETS)].index.to_list()
+    data_manager.generate_data_tables(in_min_plat_size, in_min_msg_count)
+    
+    print(f"Start: {in_start_datetime} \nEnd: {in_end_datetime}")
+    print(f"Period length: {in_end_datetime - in_start_datetime}")
+        
+    min_msg_count = in_min_msg_count
+    if in_min_msg_count is None:
+        print(data_manager.indv_actors_df["msgs_count"].value_counts().sort_index(ascending=False).cumsum())
+        x_msgcount, y_numusers = data_manager.indv_actors_df["msgs_count"].value_counts().sort_index(ascending=False).cumsum().reset_index().values.T
+        point = get_elbow(x_msgcount, y_numusers, True, f"{dataset_output_dir}/elbow_indvactors_vs_msgcount.png")
+        min_msg_count = point.x
+    
+    print(f"Min msg count per actor: {min_msg_count}")
+    print(f"Users with more than {min_msg_count} messsages posted\n", 
+          data_manager.all_users_df[data_manager.all_users_df["msgs_count"] >= min_msg_count].reset_index())
+    
+    print(f"Platform Actors with more than {in_min_plat_size} users\n", 
+          data_manager.actors_df[(data_manager.actors_df["actor_type"] == "plat") & 
+                                 (data_manager.actors_df["num_users"] >= in_min_plat_size)].reset_index())
+    
+    actor_id_list = data_manager.indv_actors_df[(data_manager.indv_actors_df["msgs_count"] >= min_msg_count)].index.to_list()
     print(f"Actors #: {len(actor_id_list)}")
-
-    # data_windows_df = ing.TransferEntropyCalculator.calculate_a_date_series(START_DATE, END_DATE, WINDOW_SHIFT_BY_DAYS, INIT_WINDOW_SIZE_BY_DAYS, AS_GROWING)
-
-    te_calculator = ing.TransferEntropyCalculator(data_manager, in_add_superclasses=False)
-
-    te_calculator.calculate_te_network_series(actor_id_list, START_DATE, END_DATE, FREQUENCY, WINDOW_SHIFT_BY_DAYS, INIT_WINDOW_SIZE_BY_DAYS, AS_GROWING)
-
-
-#
-# if __name__ == "__main__":
-#
-#     # Read input files
-#     paths = []
-#     for data_dir in DATA_DIRECTORY_LIST:
-#         paths += s3.glob(os.path.join(data_dir, "*.csv*"))
-#     paths = [f"s3://{p}" for p in paths]
-#     print(paths)
-#
-#     news_domain_classes_df = pd.read_csv(NEWS_DOMAIN_TO_CLASS_FILE,
-#                                          usecols=['Domain', 'tufm_class', 'Language'])
-#     news_domain_classes_df.rename(columns={'Domain': 'news_domain', 'tufm_class': 'class', 'Language': 'lang'},
-#                                   inplace=True)
-#
-#     # let data manager handle data
-#     data_manager = ing.DataManager("./OUTPUTS")
-#     data_manager.read_data_files(paths)
-#
-#     # preprocess
-#     data_manager.preprocess(news_domain_classes_df, START_DATE, END_DATE)
-#
-#     # generate tables
-#     min_msg_count = MIN_ACTIVITY_PER_MONTH * (END_DATE - START_DATE).days // 30
-#     print(f"minimum message count : {min_msg_count}")
-#     data_manager.generate_data_tables(MIN_PLAT_SIZE, min_msg_count)
-#
-#     print("calculating te...")
-#     te_calculator = ing.TransferEntropyCalculator(data_manager)
-#     current_start_date = START_DATE
-#     current_end_date = END_DATE
-#     te_df = te_calculator.calculate_te_network(current_start_date, current_end_date, FREQUENCY)
-#
-#     print("all done!")
-#
-#     compression_options = dict(method='zip', archive_name='actor_te_edges_df.csv')
-#     te_df.to_csv("actor_te_edges_df.csv.zip", index=False, compression=compression_options)
-#
+    
+    for AS_GROWING in [True, False]:
+            te_calculator = ing.TransferEntropyCalculator(data_manager, in_add_superclasses=False)
+            folder_type = "growing" if AS_GROWING else "moving"
+            te_calculator.calculate_te_network_series(actor_id_list, 
+                                                      in_start_datetime, 
+                                                      in_end_datetime, 
+                                                      in_frequency, 
+                                                      in_window_shift_by_days, 
+                                                      in_init_window_shift_by_days, 
+                                                      AS_GROWING, 
+                                                      os.path.join(data_manager.output_dir_path, f"dynamic/{folder_type}"))
+        
+    return True
+                                   
+if __name__ == "__main__":
+    
+    calculate_te_for_dataset("Skripal4",
+                             [f"s3://{path}" for path in s3.glob("s3://mips-main/initial_data_collection/raw_data/brandwatch/*.csv*")],
+                             datetime.datetime(2018, 3, 1).astimezone(datetime.timezone.utc),
+                             datetime.datetime(2018, 5, 1).astimezone(datetime.timezone.utc),
+                             in_frequency = '12H',
+                             in_min_plat_size = 200,
+                             in_window_shift_by_days = 4,
+                             in_init_window_shift_by_days = 4)
+    
+    print("================== SKRIPAL DONE ==================")
+    
+    calculate_te_for_dataset("Ukraine4",
+                             [f"s3://{path}" for path in s3.glob("s3://mips-phase-2/initial_query_data/raw_data/brandwatch/*.csv*")],
+                             datetime.datetime(2022, 1, 1).astimezone(datetime.timezone.utc),
+                             datetime.datetime(2022, 5, 1).astimezone(datetime.timezone.utc),
+                             in_frequency = '12H',
+                             in_min_plat_size = 500,
+                             in_window_shift_by_days = 4,
+                             in_init_window_shift_by_days = 4)
+    
+    print("================== UKRAINE DONE ==================")
+    
+    
+    
+    
